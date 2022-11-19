6b508e3b0c4ff08b6dee61303adb0f8e20c2b997
Gary Jiarui Zhang <gzhang62@terpmail.umd.edu>
2022-11-19 18:24:10 -0500

    add catch for send/recv/node failues

--- include/chord.h
+++ include/chord.h
@@ -47,9 +47,11 @@ Node *finger[NUM_BYTES_IDENTIFIER];
 struct timespec last_check_predecessor;
 struct timespec last_fix_fingers;
 struct timespec last_stabilize;
-int stabilize_ongoing = 0;
-int check_predecessor_ongoing = 0;
-int fix_fingers_ongoing = 0;
+struct timespec stabilize_ongoing;
+
+bool stabilize_ongoing = false;
+bool check_predecessor_ongoing = false;
+bool fix_fingers_ongoing = false;
 
 struct sha1sum_ctx *ctx;
 struct Callback *callback_list;
