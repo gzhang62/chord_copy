b42833789b5f2b54ec7c78877b85cebf956dfe7e
Gary Jiarui Zhang <gzhang62@terpmail.umd.edu>
2022-11-19 23:44:34 -0500

    fix fingers to iterative

--- src/chord.c
+++ src/chord.c
@@ -1247,8 +1247,10 @@ int fix_fingers() {
 	// Note: the first entry of the finger table is *the current node*
 	// TODO Bobby said that we said that usually we pick only
 	// one at a time (randomly) to pick
-	for(int i = 0; i < NUM_BYTES_IDENTIFIER; i++) {
-		send_find_successor_request(n.key + (((uint64_t)1) << (i)), CALLBACK_FIX_FINGERS, i); 
+
+	send_find_successor_request(n.key + (((uint64_t)1) << (next)), CALLBACK_FIX_FINGERS, next); 
+	if(++next > NUM_BYTES_IDENTIFIER - 1) {
+		next = 0;
 	}
 
 	// if(++next > NUM_BYTES_IDENTIFIER) {
